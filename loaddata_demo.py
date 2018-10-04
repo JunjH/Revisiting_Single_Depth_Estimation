0d8e79ea5ebd7f19d20915397e6c947f7b6aeb16
JunjH <40188741+JunjH@users.noreply.github.com>
2018-10-04 15:25:37 +0900

    Update loaddata_demo.py

--- loaddata_demo.py
+++ loaddata_demo.py
@@ -43,20 +43,3 @@ def readNyu2(filename):
 
 
     return image
-
-def readKitti(filename):
-    __imagenet_stats = {'mean': [0.485, 0.456, 0.406],
-                        'std': [0.229, 0.224, 0.225]}
-
-    image_trans = depthDataset(filename,
-                        transform=transforms.Compose([
-                        Scale([756, 228]),                   
-                        ToTensor(),                                
-                        Normalize(__imagenet_stats['mean'],
-                                 __imagenet_stats['std'])
-                       ]))
-
-    image = DataLoader(image_trans, batch_size=1, shuffle=False, num_workers=0, pin_memory=False)
-
-
-    return image
